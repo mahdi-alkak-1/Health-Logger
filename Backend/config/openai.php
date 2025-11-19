41401a7a385301b7ba4a2c743c0e69fa33c88d61
mahdi-alkak1-1 <mahdialkak1@gmail.com>
2025-11-19 13:09:31 +0200

    OpenAi: Service & config Added | entryjs & habitjs Seperated from dashboardjs | Html: dashboard Modified

--- Backend/config/openai.php
+++ Backend/config/openai.php
@@ -3,4 +3,3 @@
 const OPENAI_API_KEY = 'key';
 
 ?>
-<!-- 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 -->
